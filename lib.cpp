026d56900b43d96557eea4e8cb533044c661864c
Patelriyaaa <91497158+Patelriyaaa@users.noreply.github.com>
2023-08-31 22:06:11 -0700

    lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,3 +1,14 @@
 #include "lib.h"
 
-// Write your class implementation
+// Implement your class methods
+std::string generateNewTemp() {
+    static unsigned int tempCount = 0;
+    std::string tempName = "__temp__" + std::to_string(tempCount++);
+    return tempName;
+}
+
+std::string generateNewLabel() {
+    static unsigned int labelCount = 0;
+    std::string labelName = "__label__" + std::to_string(labelCount++);
+    return labelName;
+}
