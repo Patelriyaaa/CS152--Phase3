cf24808862cbf201e462a43f17c57f84f4702f68
Patelriyaaa <91497158+Patelriyaaa@users.noreply.github.com>
2023-08-31 22:06:50 -0700

    lib.h

--- lib.h
+++ lib.h
@@ -1,3 +1,7 @@
 #pragma once
+#include <string>
+// Declare your class members
 
-// Write your class definition here
+std::string generateNewTemp(); // Declare a function for generating temporary names
+
+std::string generateNewLabel(); // Declare a function for generating label names
